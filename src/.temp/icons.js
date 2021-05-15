197dd88e5dea164f93935f95e6d8262a906691b7
sy <7525117+huolingmengling@user.noreply.gitee.com>
2021-05-15 20:03:06 +0800

    no message

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=77a3ae1"
+      "src": "/assets/static/src/favicon.png?width=76&key=e4838c4"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=77a3ae1"
+      "src": "/assets/static/src/favicon.png?width=152&key=e4838c4"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=77a3ae1"
+      "src": "/assets/static/src/favicon.png?width=120&key=e4838c4"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=77a3ae1"
+      "src": "/assets/static/src/favicon.png?width=167&key=e4838c4"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=77a3ae1"
+      "src": "/assets/static/src/favicon.png?width=180&key=e4838c4"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=23da55d"
+      "src": "/assets/static/src/favicon.png?width=16&key=9da2928"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=23da55d"
+      "src": "/assets/static/src/favicon.png?width=32&key=9da2928"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=23da55d"
+      "src": "/assets/static/src/favicon.png?width=96&key=9da2928"
     }
   ]
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,110 @@
 <template>
   <Layout>
+    <!-- Page Header-->
+    <header class="masthead"
+            :style="`background-image: url(${GRIDSOME_API_URL+general.cover?general.cover.url:''})`">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{general.title}}</h1>
+              <span class="subheading">{{general.subtitle}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Main Content-->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview"
+               v-for="edge in $page.posts.edges"
+               :key="edge.node.id">
+            <g-link :to="'/post/'+edge.node.id">
+              <h2 class="post-title">{{edge.node.title}}</h2>
+              <!-- <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3> -->
+            </g-link>
+            <p class="post-meta">
+              Posted by
+              <a href="#!">{{edge.node.created_by.firstname}}{{edge.node.created_by.lastname}}</a>
+              on {{edge.node.created_at}}
+            </p>
+            <p>
+              <span v-for="tag in edge.node.tags">
+                <g-link :to="'/tag/'+tag.id">{{tag.title}}</g-link>&nbsp;
+              </span>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+            </p>
+          </div>
+          <hr />
+          <hr />
+          <!-- Pager-->
+          <!-- <div class="clearfix"><a class="btn btn-primary float-right"
+               href="#!">Older Posts →</a></div> -->
+          <pager :info="$page.posts.pageInfo" />
+        </div>
+      </div>
+    </div>
+    <hr />
   </Layout>
 </template>
-
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
+<page-query>
+  query($page:Int){
+    posts:allStrapiPost(perPage:2,page:$page)@paginate{
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges{
+        node{
+          id
+          title
+          created_at
+          created_by{
+            id
+            firstname
+            lastname
+          }
+          tags{
+            id
+            title
+          }
+        }
+      }
+    }
+    general:allStrapiGeneral{
+      edges{
+        node{
+          id
+          title
+          subtitle
+          cover{
+            url
+          }
+        }
+      }
+    }
   }
-}
+    
+</page-query>
+<script>
+import { Pager } from "gridsome";
+export default {
+  components: {
+    Pager,
+  },
+  computed: {
+    general() {
+      return this.$page.general.edges[0].node;
+    },
+  },
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
-}
 </style>
